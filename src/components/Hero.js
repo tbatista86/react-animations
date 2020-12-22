0ba562b8e072035f8956c77ff17dbbced390316a
Tiago Batista <tiagobatista86@gmail.com>
2020-12-22 12:02:53 -0300

    create sliders

---
+++ src/components/Hero.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import styled, { css } from 'styled-components/macro'
+import { IoMdArrowRoundForward } from 'react-icons/io'
+import { IoArrowBack, IoArrowForward } from 'react-icons/io5'
+import { Button } from './Button';
+
+const HeroSection = styled.section`
+  height: 100vh;
+  max-height: 1100px;
+  position: relative;
+  overflow: hidden;
+`;
+
+const HeroWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`;
+
+const HeroSlide = styled.div`
+
+`;
+
+const HeroSlider = styled.div`
+  
+`;
+
+const HeroImage = styled.img`
+  
+`;
+
+const HeroContent = styled.div`
+  
+`;
+
+const Arrow = styled(IoMdArrowRoundForward)`
+  
+`;
+
+const SliderButtons = styled.div`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  display: flex;
+  z-index: 10;
+`;
+
+const arrowButtons = css`
+  width: 50px;
+  height: 50px;
+  color: #fff;
+  cursor: pointer;
+  background: #000d1a;
+  border-radius: 50px;
+  padding: 10px;
+  margin-right: 1rem;
+  user-select: none;
+  transition: 0.3s;
+
+  &:hover {
+    background: #cd853f;
+    transform: scale(1.05);
+  }
+`;
+
+const PrevArrow = styled(IoArrowBack)`
+  ${arrowButtons}
+`;
+
+const NextArrow = styled(IoArrowForward)`
+  ${arrowButtons}
+`;
+
+const Hero = ({slides}) => {
+  return (
+    <HeroSection>
+      <HeroWrapper>
+          {slides.map((slide, index) => (
+            <HeroSlide key={index}>
+              <HeroSlider>
+                <HeroImage />
+                <HeroContent>
+                  <h1>{slide.title}</h1>
+                  <p>{slide.price}</p>
+                  <Button to={slide.path} primary={true} css={`max-width: 160px;`}>
+                    {slide.label}
+                    <Arrow />
+                  </Button>
+                </HeroContent>
+              </HeroSlider>
+            </HeroSlide>
+          ))}
+          <SliderButtons>
+            <PrevArrow />
+            <NextArrow />
+          </SliderButtons>
+      </HeroWrapper>
+    </HeroSection>
+  )
+}
+
+export default Hero
---
+++ src/data/SliderData.js
@@ -0,0 +1,66 @@
+import ImageOne from '../images/house1.jpg'
+import ImageTwo from '../images/house2.jpg'
+import ImageThree from '../images/house3.jpg'
+import ImageFour from '../images/house4.jpg'
+
+export const SliderData = [
+  {
+    title: 'Luxury Villa in Bali, Indonesia',
+    price: '$2,280,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageOne,
+    alt: 'Home'
+  },
+  {
+    title: 'House in San jose, Costa Rica',
+    price: '$2,300,000',
+    path: '/homes',
+    alt: 'View Home',
+    label: 'View Home',
+    image: ImageTwo,
+  },
+  {
+    title: 'House in Calabasas, California',
+    price: '$6,610,000',
+    path: '/homes',
+    alt: 'View Home',
+    label: 'View Home',
+    image: ImageThree,
+  },
+  {
+    title: 'House in Alajuela, Costa Rica',
+    price: '$5,420,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageFour,
+    alt: 'house'
+  }
+]
+
+// export const SliderDataTwo = [
+//   {
+//     title: 'House in Las Vegas, Nevada',
+//     price: '$8,980,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageFive,
+//     alt: 'house'
+//   },
+//   {
+//     title: 'House in Houston, Texas',
+//     price: '$3,460,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSix,
+//     alt: 'house'
+//   },
+//   {
+//     title: 'House in Alajuela, Costa Rica',
+//     price: '$5,420,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSeven,
+//     alt: 'house'
+//   }
+// ]
